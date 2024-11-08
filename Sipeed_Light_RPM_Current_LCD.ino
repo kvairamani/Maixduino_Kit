cf3c69ef98046bf7b44b90abcdb5b0cb882ea0ca
kvairamani <44131790+kvairamani@users.noreply.github.com>
2024-11-08 11:12:49 +0530

    Update Sipeed_Light_RPM_Current_LCD.ino

--- Sipeed_Light_RPM_Current_LCD.ino
+++ Sipeed_Light_RPM_Current_LCD.ino
@@ -1,10 +1,19 @@
+/*
+This code is tested working with Sipeed Maixduino
+it measures following parameters:
+1. light intensity (BH1750 sensor),
+2. temperature & Humidity (DHT11 sensor), 
+3. current and power consumption of DC motor (INA219 module)
+4. DC Motor Rotation per minute (Opto interruptor sensor reads DC motor rotation through the 30 slot mechanism. DC motor connected with L293N module)  
+all the measured parameters values are updated in the Sipeed ST7789 display and transferred via serial port at 115200 baud rate.
+Modified and updated by K. Vairamani on 08/11/2024
+*/
 #include <Sipeed_ST7789.h>
 #include<BH1750.h>
 #include<Wire.h>
 #include "INA219.h"
 #include <DHT11.h>
 DHT11 dht11(3);
-
 INA219 INA(0x41);
 BH1750 lightMeter;
 unsigned long start_time = 0;
@@ -27,7 +36,6 @@ Sipeed_ST7789 lcd(320, 240, spi_);
 int PWM_EN = 6; //Connected to enable pin for L298N module
 int M1 = 8; // Connected to IN1 of L298N module
 int M2 = 9; // Connected to IN2 of L298N module
-int SW = 7; //Switch to turn on motor
 
 //funtion protypes
 void title();
@@ -41,7 +49,6 @@ void readrpm();
 void readINA();
 void temphumi();
 
-
 void setup()
 {
     Wire.begin();
@@ -49,9 +56,6 @@ void setup()
     motorinit();
     motoroff(); 
     pinMode(counter, INPUT_PULLUP);
-    //pinMode(sensor,INPUT_PULLUP);
-    //pinMode(SW, INPUT);
-  //  attachInterrupt(digitalPinToInterrupt(counter), count, RISING);
     lightMeter.begin();
     if (!INA.begin() )
     {
@@ -68,29 +72,11 @@ void loop() {
   title();
   readlight();  
   temphumi();
-  
-  Serial.println("Duty Cycle 100%");
-  RPM_speed(255);
+  RPM_speed(255); //Duty Cycle 100%
   motoronforward();
   readINA();
   readrpm();
-  motoroff();
-  delay(1000);
-
-  Serial.println("Duty Cycle 50%");
-  RPM_speed(127);
-  motoronforward();
-  readINA();
-  readrpm();
-  motoroff();
-  delay(1000);
-
-  Serial.println("Duty Cycle 25%");
-  RPM_speed(63);
-  motoronforward();
-  readINA();
-  readrpm();
-  motoroff();
+//  motoroff();
   delay(1000);
 }
 
@@ -152,12 +138,14 @@ void readrpm()
 void readlight()
 {
   float lux = lightMeter.readLightLevel();
+  Serial.print("Light lx:");
+  Serial.print(lux,2);
+  Serial.print(',');
   lcd.setCursor(1,70);
   lcd.print("Light lx: ");
   lcd.setCursor(180,70);
   lcd.println(lux);
-  //lcd.println(" lx");
-
+  
 }
 void title()
 {
@@ -183,29 +171,27 @@ void readINA()
   lcd.print("Power mW:");
   lcd.setCursor(180,170);
   lcd.print(INA.getPower_mW(), 2);
-  //delay(500);
-  Serial.print(INA.getShuntVoltage_mV(), 2);
-  Serial.print("\t\t");
+  Serial.print("Current mA:");
   Serial.print(INA.getCurrent_mA(), 2);
-  Serial.print("\t\t");
+  Serial.print(",");
+  Serial.print("Power mW:");
   Serial.print(INA.getPower_mW(), 2);
-  Serial.println("\t\t");
-  //delay(500);
+  Serial.print(",");
 }
 void temphumi()
 {
       // Attempt to read the temperature and humidity values from the DHT11 sensor.
     int result = dht11.readTemperatureHumidity(temperature, humidity);
-
     // Check the results of the readings.
     // If the reading is successful, print the temperature and humidity values.
     // If there are errors, print the appropriate error messages.
     if (result == 0) {
         Serial.print("Temperature °C: ");
-        Serial.println(temperature);
-        
+        Serial.print(temperature);
+        Serial.print(',');
         Serial.print("Humidity %:");
-        Serial.println(humidity);
+        Serial.print(humidity);
+        Serial.print(',');
         lcd.setCursor(1,90);
         lcd.print("Temperature C: ");
         lcd.setCursor(180,90);
